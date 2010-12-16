1ff7a97bdbdbbf1793cf640d101b3a6438d9c957
Zbigniew Lukasiak <zby@cpan.org>
2010-12-16 10:53:59 +0100

    removed site template variable

--- nblog.pl
+++ nblog.pl
@@ -2,11 +2,6 @@
   name => 'Nblog',
   static_root => '',
   using_frontend_proxy => 0,
-  site => {
-    template => 'default',
-    name => 'Writers Unite!',
-    description => 'a writers blog',
-  },
   'schema' => {
     connect_info => [
         'dbi:SQLite:dbname=./nblog.db',
