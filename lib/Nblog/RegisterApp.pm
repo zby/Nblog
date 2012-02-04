53850ad7a734b3fd4d43092f8a2503e58a0d5f51
Zbigniew Lukasiak <zby@cpan.org>
2012-02-04 11:40:06 +0100

    using WebPrototypes::Registration

--- lib/Nblog/RegisterApp.pm
+++ lib/Nblog/RegisterApp.pm
@@ -2,16 +2,9 @@ use strict;
 use warnings;
 
 package Nblog::RegisterApp;
-use parent qw(Plack::Component);
+use parent qw(WebPrototypes::Registration);
 
-use String::Random 'random_regex';
-use Email::Valid;
-use Plack::Util::Accessor qw( schema renderer email_validator );
-
-sub prepare_app {
-    my $self = shift;
-    $self->email_validator( Email::Valid->new() ) if !defined $self->email_validator;
-}
+use Plack::Util::Accessor qw( renderer schema );
 
 sub find_user {
     my( $self, $name ) = @_;
@@ -28,45 +21,6 @@ sub create_user{
     my( $self, %fields ) = @_;
     return $self->schema->resultset( 'User' )->create( { %fields } );
 }
-
-sub build_reply{
-    my( $self, $text ) = @_;
-    return [ 200, [ 'Content-Type' => 'text/html' ], [ $self->wrap_text( $text ) ] ];
-}
-
-sub call {
-    my($self, $env) = @_;
-    my $req = Plack::Request->new( $env );
-    my $uerror = '';
-    my $eerror = '';
-    my $username = '';
-    my $email = '';
-    if( $req->method eq 'POST' ){
-        $username = $req->param( 'username' );
-        $email = $req->param( 'email' );
-        if( $self->find_user( $username ) ){
-            $uerror = '<span class="error">This username is already registered</span>';
-        }
-        if( !$self->email_validator->address( $email ) ){
-            $eerror = '<span class="error">Wrong format of email</span>';
-        }
-        if( !$uerror && !$eerror ){
-            my $user = $self->create_user( username => $username, email => $email, pass_token => random_regex( '\w{40}' ) );
-            $self->_send_mail_token( $env, $user, $username, $email );
-            return $self->build_reply( "Email sent" );
-        }
-    }
-    return $self->build_reply( <<END );
-<form method="POST">
-Username: <input type="text" name="username" value="$username"> $uerror
-Email: <input type="text" name="email" value="$email"> $eerror
-<input type="submit">
-</form>
-END
-
-}
-
-sub _send_mail_token {}
 
 1;
 
--- t/register.t
+++ t/register.t
@@ -6,6 +6,12 @@ use Nblog;
 use Test::WWW::Mechanize::PSGI;
 use Plack::Builder;
 
+my $email_log;
+local *Nblog::RegisterApp::send_mail = sub {
+    my( $self, $mail ) = @_;
+    $email_log .= $mail->as_string;
+};
+
 my $app = Nblog->new_with_config();
 
 my $mech = Test::WWW::Mechanize::PSGI->new( app => $app->psgi_app );
@@ -33,5 +39,7 @@ $mech->submit_form_ok( {
 $mech->content_contains( 'Email sent', );
 my $user = $app->schema->resultset( 'User' )->search({ username =>  'new_user', email => 'test@example.com' })->first;
 ok( $user, 'User created' );
+my $email_addr = $user->email;
+like( $email_log, qr/To: $email_addr/, "Email to test user sent" );
 
 done_testing;
